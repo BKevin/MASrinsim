90ff835275bbb1500ad4cbed094a84ae82cfc458
Pieter Laenen <pieterlaenen@live.be>
2016-05-10 22:16:07 +0200

    Add penalty calculation stub

---
+++ src/main/java/main/PenaltyUtil.java
@@ -0,0 +1,25 @@
+package main;
+
+import com.github.rinde.rinsim.core.model.time.TimeLapse;
+
+import java.util.List;
+
+/**
+ * Created by pieter on 09.05.16.
+ */
+public class PenaltyUtil {
+
+    public static double calculateParcelPenalty(MyParcel parcel, List<MyParcel> planning){
+
+
+
+
+        return 0.0;
+    }
+
+    private static double calculateParcelPenalty(MyParcel parcel, TimeLapse time){
+        return 0.0;
+    }
+
+
+}
