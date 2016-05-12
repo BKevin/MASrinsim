0e35171a9e358f81bbd86513aad225d70e3e7dca
Pieter Laenen <pieterlaenen@live.be>
2016-05-12 09:26:02 +0200

    Change MyVehicle parent to RouteFollowingVehicle
    RouteFollowingVehicle already implements the routing protocol we want. It is only natural to let our Vehicle use the existing features.

--- src/main/java/main/MyVehicle.java
+++ src/main/java/main/MyVehicle.java
@@ -2,53 +2,54 @@ package main;
 
 import com.github.rinde.rinsim.core.model.comm.*;
 import com.github.rinde.rinsim.core.model.pdp.PDPModel;
-import com.github.rinde.rinsim.core.model.pdp.TimeWindowPolicy;
-import com.github.rinde.rinsim.core.model.pdp.Vehicle;
+import com.github.rinde.rinsim.core.model.pdp.Parcel;
 import com.github.rinde.rinsim.core.model.pdp.VehicleDTO;
 import com.github.rinde.rinsim.core.model.road.RoadModel;
 import com.github.rinde.rinsim.core.model.time.TimeLapse;
 import com.github.rinde.rinsim.geom.Point;
+import com.github.rinde.rinsim.pdptw.common.RouteFollowingVehicle;
 import com.google.common.base.Optional;
 import comm.AcceptBidMessage;
 import comm.AuctionedParcelMessage;
 import comm.BidMessage;
+import comm.RefuseBidMessage;
 
-import java.sql.Time;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by KevinB on 8/05/2016.
  */
-public class MyVehicle extends Vehicle implements CommUser{
+public class MyVehicle extends RouteFollowingVehicle implements CommUser{
 
-    private Optional<RoadModel> roadModel;
-    private Optional<PDPModel>  pPdpModel;
+
     private Optional<CommDevice> device;
-    private List<MyParcel> parcels;
+
+    private Map<Parcel, Integer> calculatedIndexOfParcel;
 
     public MyVehicle(Point startPosition, int vehicleCapacity, double speed) {
         super(VehicleDTO.builder()
                 .startPosition(startPosition)
                 .capacity(vehicleCapacity)
                 .speed(speed)
-                .build());
-        parcels = new LinkedList<MyParcel>();
+                .build(),
+                true); //diversion is possible (changing the parcel we move towards mid-movement)
+
     }
 
+    /**
+     * Pre-tick message handling.
+     *
+     * May alter route when parcel is won.
+     *
+     * @param time
+     */
     @Override
-    public void initRoadPDP(RoadModel pRoadModel, PDPModel ppPdpModel) {
-        roadModel = Optional.of(pRoadModel);
-        pPdpModel = Optional.of(ppPdpModel);
-    }
-
-    @Override
-    protected void tickImpl(TimeLapse timeLapse) {
+    protected void preTick(TimeLapse time) {
+        super.preTick(time);
 
         communicate();
-
-        move(timeLapse);
-
     }
 
     private void communicate() {
@@ -56,9 +57,11 @@ public class MyVehicle extends Vehicle implements CommUser{
             return;
         //check new messages
         boolean haveToRecalculate = false;
+
         for(Message message : device.get().getUnreadMessages()){
             //if AuctionedParcelMessage then calculate bid and send BidMessage
             final MessageContents contents = message.getContents();
+
             if(contents instanceof AuctionedParcelMessage){
                 AuctionedParcelMessage auctionedParcelMessage = (AuctionedParcelMessage) contents;
                 //TODO change to asynchonous call(?)
@@ -67,9 +70,26 @@ public class MyVehicle extends Vehicle implements CommUser{
             }
             //if AcceptBidMessage then add parcel to todolist
             if(contents instanceof AcceptBidMessage){
+                // The Vehicle won the auction.
                 AcceptBidMessage acceptedBidMessage = (AcceptBidMessage) contents;
-                parcels.add(acceptedBidMessage.getParcel());
-                haveToRecalculate = true;
+
+                // Add vehicle to route
+                LinkedList newRoute = new LinkedList<>(this.getRoute());
+
+                newRoute.add(
+                        calculatedIndexOfParcel.get(acceptedBidMessage.getParcel()),
+                        acceptedBidMessage.getParcel());
+
+                this.setRoute(newRoute);
+
+            }
+
+            if(contents instanceof RefuseBidMessage){
+
+                RefuseBidMessage refuseBidMessage = (RefuseBidMessage) contents;
+
+                calculatedIndexOfParcel.remove(refuseBidMessage.getParcel());
+
             }
 
             //if RefuseBidMessage then remove from memory (incase of reauctioned)
@@ -100,52 +120,53 @@ public class MyVehicle extends Vehicle implements CommUser{
     }
 
 
+    @Deprecated
     private void move(TimeLapse time) {
         //move in the direction of the first scheduled parcel
         //if arrived then pickup or deliver Parcel
-        final RoadModel rm = getRoadModel();
-        final PDPModel pm = getPDPModel();
-
-        if (!time.hasTimeLeft()) {
-            return;
-        }
-
-        if (parcels.isEmpty()) {
-            //do nothing when no assignment
-            return;
-        }
-
-        if (!parcels.isEmpty()) {
-            MyParcel curr = parcels.get(0);
-            final boolean inCargo = pm.containerContains(this, curr);
-            // sanity check: if it is not in our cargo AND it is also not on the
-            // RoadModel, we cannot go to curr anymore.
-            if (!inCargo && !rm.containsObject(curr)) {
-                throw new IllegalStateException("A Parcel isn't available, but MyVehicle still has to PDP it.");
-            } else if (inCargo) {
-                // if it is in cargo, go to its destination
-                rm.moveTo(this, curr.getDeliveryLocation(), time);
-                if (rm.getPosition(this).equals(curr.getDeliveryLocation())) {
-                    // deliver when we arrive
-                    pm.deliver(this, curr, time);
-                    parcels.remove(0);
-                }
-            } else {
-                // it is still available, go there as fast as possible
-                rm.moveTo(this, curr, time);
-                if (rm.equalPosition(this, curr)) {
-                    // pickup customer
-                    pm.pickup(this, curr, time);
-                }
-            }
-        }
+//        final RoadModel rm = getRoadModel();
+//        final PDPModel pm = getPDPModel();
+//
+//        if (!time.hasTimeLeft()) {
+//            return;
+//        }
+//
+//        if (parcels.isEmpty()) {
+//            //do nothing when no assignment
+//            return;
+//        }
+//
+//        if (!parcels.isEmpty()) {
+//            MyParcel curr = parcels.get(0);
+//            final boolean inCargo = pm.containerContains(this, curr);
+//            // sanity check: if it is not in our cargo AND it is also not on the
+//            // RoadModel, we cannot go to curr anymore.
+//            if (!inCargo && !rm.containsObject(curr)) {
+//                throw new IllegalStateException("A Parcel isn't available, but MyVehicle still has to PDP it.");
+//            } else if (inCargo) {
+//                // if it is in cargo, go to its destination
+//                rm.moveTo(this, curr.getDeliveryLocation(), time);
+//                if (rm.getPosition(this).equals(curr.getDeliveryLocation())) {
+//                    // deliver when we arrive
+//                    pm.deliver(this, curr, time);
+//                    parcels.remove(0);
+//                }
+//            } else {
+//                // it is still available, go there as fast as possible
+//                rm.moveTo(this, curr, time);
+//                if (rm.equalPosition(this, curr)) {
+//                    // pickup customer
+//                    pm.pickup(this, curr, time);
+//                }
+//            }
+//        }
     }
 
 
     @Override
     public Optional<Point> getPosition() {
-        if (roadModel.get().containsObject(this)) {
-            return Optional.of(roadModel.get().getPosition(this));
+        if (this.getRoadModel().get().containsObject(this)) {
+            return Optional.of(this.getRoadModel().get().getPosition(this));
         }
         return Optional.absent();
     }
@@ -156,10 +177,6 @@ public class MyVehicle extends Vehicle implements CommUser{
     }
 
 
-
-    public List<MyParcel> getPlanning(){
-        return this.parcels;
-    }
 
     public void getPenalty(){
 
