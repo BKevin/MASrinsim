477a7cfe64a54cfa335c3eea6621df23929614b9
Pieter Laenen <pieterlaenen@live.be>
2016-06-01 16:01:49 +0200

    Extend logging to CbgaAgent

--- src/mas/cbba/Debug.java
+++ src/mas/cbba/Debug.java
@@ -4,6 +4,7 @@ import com.github.rinde.rinsim.core.model.pdp.PDPModel;
 import com.github.rinde.rinsim.core.model.pdp.Parcel;
 import mas.cbba.agent.AbstractConsensusAgent;
 import mas.cbba.agent.CbbaAgent;
+import mas.cbba.agent.CbgaAgent;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -33,6 +34,31 @@ public class Debug {
 
     }
 
+    public static void logParcelListForAgent(CbgaAgent agent, Map<Parcel, PDPModel.ParcelState> states, Collection<Parcel> availableParcels){
+        Logger logger = LoggerFactory.getLogger(agent.getClass());
+
+        Set<Parcel> parcels = agent.getX().rowKeySet();
+
+        logger.info("Parcels for {} (Parcels:{} - Available:{}):  \n{}",
+                agent,
+                parcels.size(),
+                availableParcels.size(),
+                agent.getX());
+
+        logger.info("Parcel states: {}", states);
+
+
+    }
+
+    public static void logRouteForAgent(AbstractConsensusAgent agent, Map<Parcel, PDPModel.ParcelState> states){
+        if(agent instanceof CbbaAgent){
+            logRouteForAgent((CbbaAgent)agent, states);
+        }
+        else if(agent instanceof CbgaAgent) {
+            logRouteForAgent((CbgaAgent) agent, states);
+        }
+    }
+
     public static void logRouteForAgent(CbbaAgent agent, Map<Parcel, PDPModel.ParcelState> states){
         Logger logger = LoggerFactory.getLogger(agent.getClass());
 
@@ -40,6 +66,20 @@ public class Debug {
                 agent,
                 agent.getZ().keySet().size(),
                 agent.getZ().keySet(),
+                agent.getRoute());
+
+        logger.info("Parcel states: {}", states);
+
+
+    }
+
+    public static void logRouteForAgent(CbgaAgent agent, Map<Parcel, PDPModel.ParcelState> states){
+        Logger logger = LoggerFactory.getLogger(agent.getClass());
+
+        logger.info("Route for {} (Parcels:{}) built from {}: \n {}",
+                agent,
+                agent.getX().rowKeySet().size(),
+                agent.getX().rowKeySet(),
                 agent.getRoute());
 
         logger.info("Parcel states: {}", states);
